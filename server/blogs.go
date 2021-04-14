2eb26de05d274ccaf9393b98e49c2b8e639385df
singhayushh <coding.ayush@gmail.com>
2021-04-14 14:53:22 +0530

    added: reading and exposing endpoints from json

--- server/blogs.go
+++ server/blogs.go
@@ -23,24 +23,12 @@ type Blog struct {
 // GetBlogs ...
 func GetBlogs() Blogs {
 	jsonFile, err := os.Open("./client/blogs.json")
-	// if we os.Open returns an error then handle it
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println("Successfully Opened users.json")
-
-	// read our opened jsonFile as a byte array.
 	byteValue, _ := ioutil.ReadAll(jsonFile)
-
-	// we initialize our Users array
 	var blogs Blogs
-
-	// we unmarshal our byteArray which contains our
-	// jsonFile's content into 'users' which we defined above
 	json.Unmarshal(byteValue, &blogs)
-
-	// defer the closing of our jsonFile so that we can parse it later on
 	defer jsonFile.Close()
-
 	return blogs
 }
