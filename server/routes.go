14373a3ad79879b0ad291981235b1b0b0c6cf000
MANGALAM SHARMA <78803404+Mangalam0512@users.noreply.github.com>
2021-06-17 01:24:57 +0530

    About page added

--- server/routes.go
+++ server/routes.go
@@ -4,6 +4,7 @@ var blogs = GetBlogs()
 
 func (server *Server) initRoutes() {
 	server.Router.GET("/", RenderHome)
+	server.Router.GET("/about", RenderAboutUs)
 	for i := 0; i < len(blogs.Blogs); i++ {
 		endpoint := "/" + blogs.Blogs[i].URL
 		server.Router.GET(endpoint, RenderBlog(blogs.Blogs[i].Title, blogs.Blogs[i].Content))
