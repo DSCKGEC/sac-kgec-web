860cc6671d03ce095e21af20034c6e535e526133
singhayushh <coding.ayush@gmail.com>
2021-06-21 16:00:05 +0530

    minor enhancements and bug fixes

--- client/js/header.js
+++ client/js/header.js
@@ -1,15 +1,8 @@
-const Slidingnav = () => {
-
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".navlinks");
-    const navlinks = document.querySelector(".navlinks li");
-    // Toggle nav
-    burger.addEventListener("click", () => {
-        nav.classList.toggle("navactive");
-    });
-    // animate links
-    // navlinks.forEach((link, index) => { console.log(index); });
-
-}
-
-Slidingnav();
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".navlinks");
+const navlinks = document.querySelector(".navlinks li");
+// Toggle nav
+burger.addEventListener("click", () => {
+    console.log('clicked');
+    nav.classList.toggle("navactive");
+});
